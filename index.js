afb8ef376e126f42da8b7a5862d8e7c648199200
Shahrukh1505 <shahrukhansari15@gmail.com>
2021-08-20 15:02:27 +0530

    commit before final changes

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-#!/usr/bin/env node
-
+#!/usr/bin/env node 
+//node is an environment wcat command will run through this environment
+//generally, globally linking data are stored in bin
+/*executes index.js through wcat it will be globally accessible */
 //require import statement
 //fs library code stored in fs
+/*To handle file operations like creating, reading, deleting, etc.,
+ Node.js provides an inbuilt module called FS (File System).
+ Module- provides special functionality , code can be used multiple times */
+ /*Common use for File System module:
+
+Read Files
+Write Files
+Append Files
+Close Files
+Delete Files */
 const fs = require("fs");
 
+
 //process.arg node path nd file path on first two indexes + the arguments that you.ve passed
+/*The process.argv property is an inbuilt application programming interface of the process module 
+which is used to get the arguments passed to the node.js process when run in the command line.
+
+Syntax:
+
+process.argv
+Return Value: This property returns an array containing the arguments passed to the process when run it in the command line. 
+The first element is the process execution path(node path) and the second element is the path for the js file. */
+
 let arguments = process.argv.slice(2);
 //it will contain all the flags
 let flags = [];
@@ -26,49 +48,120 @@ flags.push(i);  //push the flags in flag array
         filenames.push(i); //push the file
     }
 }
-//if no flags and only filenames are there in the arguent then
-// if(flags.length == 0 && filenames.length!=0){
-// for(let file of filenames) {
-//     console.log(fs.readFileSync(file,"utf-8"));
-// }
-// }
-// else {
-//  for(let flag of flags){
-//      //-rs flag from removing spaces
-//      if(flag == "-rs"){
-//          for(let file of filenames){
-//              let fileData = fs.readFileSync(file,"utf-8");
-//              console.log(fileData.split(" ").join(""));
-//              //split the contents of file in array
-//              //then join the array as one string
-//          }
-//      }
-//  }
-// }
+
 
 //optimised
 for(let file of filenames){
     let fileData = fs.readFileSync(file,"utf-8");
     for(let flag of flags) {
         if(flag == "-rs"){ //remove spaces
+          
             fileData = removeAll(fileData, " ");      
          }
          if(flag == "-rn"){ //remove new line
+            
              fileData = removeAll(fileData, "\r\n");
          }
          if(flag == "-rsc"){
             for(let secondaryArgument of secondaryArguments){
                 //for removing special characters passed by us using $ (\r\n nahi remove kr skte)
                 //$andthetexttobereoved
+                
                 fileData = removeAll(fileData, secondaryArgument);
             }
-    }
+        }
+            if(flag == "-w"){
+               
+            fs.writeFileSync(file, "hello");
+                
+            }
+            if(flag == '-s'){
+                // add 1,2,3 when a new line
+              fileData = addNewline(fileData);
+            }
 
- 
-}
+            if(flag == '-sn'){
+            fileData = addNewline1(fileData);
+            }
+            if(flag == '-rel'){
+fileData = removeExtraLine(fileData);
+            }
+            if(flag == '-cli'){
+                //count no. of lines
+fileData = countLines(fileData);
+            }
+            if(flag == '-cw'){
+                //count no. of words
+                fileData = countWords(fileData);
+            }
+    }
 console.log(fileData);
 }
 /*executes index.js through wcat it will be globally accessible */
 function removeAll(string, removalData){
     return string.split(removalData).join("");
 }
+//adding numbers in front of new lines
+//we first split the string by new line into array and then add number at each of the element of the array
+function addNewline(string){
+    let contentArr = string.split("\r\n");
+    let j = 1;
+    for(let i = 0;i<contentArr.length;i++){
+        contentArr[i] = `${j}  ${contentArr[i]}`;
+        j++;
+    }
+    return contentArr.join("\r\n");
+}
+//adding numbers in front of new line
+//omitting the empty lines
+
+//We first split the string content by new line , after that we iterate over each content of the array and if it's empty or not
+//if it's empty then don't add the number else if it's not add the number
+
+function addNewline1(string){
+    let contentArr = string.split("\r\n");
+    let j = 1;
+    for(let i = 0;i<contentArr.length;i++){
+        if(contentArr[i] != ""){
+        contentArr[i] = `${j}  ${contentArr[i]}`;
+        j++;
+        }
+    }
+    return contentArr.join("\r\n");
+}
+
+function removeExtraLine(string){
+
+    let contentArr = string.split("\r\n");
+
+    let data = [];
+
+    for(let i = 1;i<contentArr.length;i++){
+     if(contentArr[i] == "" && contentArr[i-1] == ""){
+         contentArr[i] = null;
+     }
+     else if(contentArr[i] == "" && contentArr[i-1] == null){
+         contentArr[i] = null;
+     }
+
+    }
+
+    for(let i = 0;i<contentArr.length;i++){
+        if(contentArr[i]!=null){
+            data.push(contentArr[i]);
+        }
+    }
+
+    return data.join("\r\n");
+}
+
+
+//counting the number of lines in file
+
+function countLines(string){
+    let contentArr = string.split("\r\n");
+
+    return contentArr.length;
+}
+
+
